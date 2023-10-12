b47c4b3043b9aa2e0fa3111c2b7da6e5c9b22369
marinlauber <marinlauber@tudelft.nl>
2023-10-12 16:40:29 +0200

    nurbs flow example

--- TwoD_nurbs.jl
+++ TwoD_nurbs.jl
@@ -1,43 +1,73 @@
 using WaterLily
+using ParametricBodies
 using StaticArrays
-include("../examples/TwoD_plots.jl")
-
-function get_omega!(sim)
-    body(I) = sum(WaterLily.ϕ(i,CartesianIndex(I,i),sim.flow.μ₀) for i ∈ 1:2)/2
-    @inside sim.flow.σ[I] = WaterLily.curl(3,I,sim.flow.u) * body(I) * sim.L / sim.U
-end
-
-plot_vorticity(ω; limit=maximum(abs,ω)) =contourf(clamp.(ω,-limit,limit)',dpi=300,
-    color=palette(:RdBu_11), clims=(-limit, limit), linewidth=0,
-    aspect_ratio=:equal, legend=false, border=:none)
+include("examples/TwoD_plots.jl")
 
 # parameters
-L=2^5
+L=2^4
 Re=250
-U=1;amp=π/4
+U =1
 ϵ=0.5
 thk=2ϵ+√2
 
-# make sim
-Points = 3L.+0.5*L*Array(reduce(hcat,[SVector(i-5, sin(i^2)) for i in 1:9])')
-# Points = 3L.+0.5*L*Array(reduce(hcat,[SVector(i-5, sin(i^2)) for i in 1:9])')
-# Points = Array([[3L,3L] [2L,3L] [3L,4L]]')
-Body = SplineBody(Points;deg=2,thk=thk,compose=false,T=Float64)
-sim = Simulation((8L,6L),(U,0),L;U,ν=U*L/Re,body=Body,ϵ,T=Float64)
+# overload the distance function
+ParametricBodies.dis(p,n) = √(p'*p) - thk/2
 
-# sim
-t₀ = round(sim_time(sim))
-duration = 15; tstep = 0.1;
-@time @gif for tᵢ in range(t₀,t₀+duration;step=tstep)
+# # NURBS points, weights and knot vector for a circle
+# cps = SA[1 1 0 -1 -1 -1  0  1 1
+#          0 1 1  1  0 -1 -1 -1 0]*L/2 .+ [2L,3L]
+# cps_m = MMatrix(cps)
+# weights = SA[1.,√2/2,1.,√2/2,1.,√2/2,1.,√2/2,1.]
+# knots =   SA[0,0,0,1/4,1/4,1/2,1/2,3/4,3/4,1,1,1]
+
+cps = SA[-1 0 1
+         0.5 0.25 0]*L .+ [2L,3L]
+cps_m = MMatrix(cps)
+weights = SA[1.,1.,1.]
+knots =   SA[0,0,0,1,1,1.]
+
+# make a nurbs curve
+circle = NurbsCurve(copy(cps_m),knots,weights)
+
+# some motion
+static(x,t) = x
+heave(x,t) = x .- SA[0,L*sin(t*U/L)]
+
+# make a body and a simulation
+# Body = ParametricBody(circle,(0,1);map=static)
+Body = DynamicBody(circle,(0,1))
+sim = Simulation((8L,6L),(U,0),L;U,ν=U*L/Re,body=Body,T=Float64)
+
+# intialize
+t₀ = sim_time(sim)
+duration = 10
+tstep = 0.1
+
+# run
+anim = @animate for tᵢ in range(t₀,t₀+duration;step=tstep)
 
     # update until time tᵢ in the background
-    sim_step!(sim,tᵢ,remeasure=false)
+    # sim_step!(sim,tᵢ,remeasure=false)
+    t = sum(sim.flow.Δt[1:end-1])
+    while t < tᵢ*sim.L/sim.U
+        # random update
+        Body.surf.pnts .=  SA[-1 0 1
+                              0.5 0.25+0.5*sin(π/4*t/sim.L) 0]*L .+ [2L,3L]
+        ParametricBodies.update!(Body,t)
+        measure!(sim,t)
+        mom_step!(sim.flow,sim.pois) # evolve Flow
+        t += sim.flow.Δt[end]
+    end
 
     # flood plot
     get_omega!(sim);
     plot_vorticity(sim.flow.σ, limit=10)
-    Plots.plot!(Body.pts[:,1],Body.pts[:,2],markers=:o,legend=false)
+    Xs = reduce(hcat,[Body.surf(s,0.0) for s ∈ 0:0.01:1])
+    plot!(Xs[1,:].+0.5,Xs[2,:].+0.5,color=:black,lw=thk,legend=false)
+    plot!(Body.surf.pnts[1,:].+0.5,Body.surf.pnts[2,:].+0.5,marker=:circle,legend=:none)
 
     # print time step
     println("tU/L=",round(tᵢ,digits=4),", Δt=",round(sim.flow.Δt[end],digits=3))
 end
+# save gif
+gif(anim, "circle_flow.gif", fps=24)

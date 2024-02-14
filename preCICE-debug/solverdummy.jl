c9a09b48772c36c48aad96b9085d177ee17bd642
marinlauber <marinlauber@tudelft.nl>
2024-02-14 17:03:30 +0100

    fix global mesh access in v3.0.0

--- preCICE-debug/solverdummy.jl
+++ preCICE-debug/solverdummy.jl
@@ -1,4 +1,27 @@
 using PreCICE
+# void Participant::getMeshVertexIDsAndCoordinates(::precice::string_view    meshName,
+#                                                  ::precice::span<VertexID> ids,
+#                                                  ::precice::span<double>   coordinates) const
+function getMeshVertexIDsAndCoordinates(
+    meshName::String,
+)::Tuple{AbstractArray{Integer},AbstractArray{Float64}}
+    @warn "The function getMeshVertexIDsAndCoordinates is still experimental"
+
+    _size = getMeshVertexSize(meshName)
+    vertexIDs = zeros(Cint, _size)
+    vertexCoordinates = zeros(Float64, _size * getMeshDimensions(meshName))
+    ccall(
+        (:precicec_getMeshVertexIDsAndCoordinates, "libprecice"),
+        Cvoid,
+        (Ptr{Int8}, Cint, Ref{Cint}, Ref{Cdouble}),
+        meshName,
+        _size,
+        vertexIDs,
+        vertexCoordinates,
+    )
+    return vertexIDs,
+    permutedims(reshape(vertexCoordinates, (_size, getMeshDimensions(meshName))))
+end
 
 commRank = 0
 commSize = 1
@@ -13,13 +36,14 @@ end
 configFileName = ARGS[1]
 solverName = ARGS[2]
 
-
 if solverName == "SolverOne"
     meshName = "SolverOne-Mesh"
+    meshOther = "SolverTwo-Mesh"
     dataWriteName = "Data-One"
     dataReadName = "Data-Two"
 else
     meshName = "SolverTwo-Mesh"
+    meshOther = "SolverOne-Mesh"
     dataReadName = "Data-One"
     dataWriteName = "Data-Two"
 end
@@ -39,17 +63,22 @@ writeData = zeros(numberOfVertices, dimensions)
 vertices = Array{Float64,2}(undef, numberOfVertices, dimensions)
 
 for i = 1:numberOfVertices, j = 1:dimensions
-    vertices[i, j] = i
+    vertices[i, j] = 2rand()-1 # spaced in [-1,1]
 end
 
 vertexIDs = PreCICE.setMeshVertices(meshName, vertices)
 
 let # setting local scope for dt outside of the while loop
-
+    
+    PreCICE.setMeshAccessRegion(meshName,[-1. 1.; -1. 1.])
     PreCICE.initialize()
 
-    PreCICE.setMeshAccessRegion(meshName,[[0, 1], [0, 1]])
-    println(PreCICE.getMeshVertexIDsAndCoordinates(meshName))
+    # get the coordinates of the nodses
+    (IDother, CoordsOther) = getMeshVertexIDsAndCoordinates(meshOther)
+    println("my coordinates")
+    @show vertexIDs, vertices
+    println("the other's coordinates")
+    @show IDother, CoordsOther
 
     while PreCICE.isCouplingOngoing()
 
